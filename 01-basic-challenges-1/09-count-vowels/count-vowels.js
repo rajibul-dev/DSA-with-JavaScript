640f0d783ebcea3ad85c86a3d8aa2dfab5b3da57
Rajibul Islam <rajibulski110@gmail.com>
2024-01-07 01:32:45 +0530

    my solution to count-vowels function challenge

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,16 @@
-function countVowels() {}
+function countVowels(string) {
+  const vowels = ["a", "e", "i", "o", "u"];
+  const lowercaseStr = string.toLowerCase();
+
+  let result = 0;
+
+  lowercaseStr.split("").forEach((char) => {
+    vowels.forEach((vowel) => {
+      char.includes(vowel) ? result++ : result;
+    });
+  });
+
+  return result;
+}
 
 module.exports = countVowels;
