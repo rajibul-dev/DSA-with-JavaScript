17f1bb6d2f6a16b9fcc5f766fc2d2e214637108f
Rajibul Islam <rajibulski110@gmail.com>
2024-01-07 01:50:38 +0530

    my solution to the remove duplicates

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,11 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+  const result = [];
+
+  arr.forEach((value) => {
+    !result.includes(value) ? result.push(value) : null;
+  });
+
+  return result;
+}
 
 module.exports = removeDuplicates;
