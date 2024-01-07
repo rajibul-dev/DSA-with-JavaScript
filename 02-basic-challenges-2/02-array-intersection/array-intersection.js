6e223f449b255d058cc7ecd87be38088e3e3fcaf
Rajibul Islam <rajibulski110@gmail.com>
2024-01-07 22:45:31 +0530

    my solution to array intersection

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,16 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  const noDuplicateArr1 = Array.from(new Set(arr1));
+  const noDuplicateArr2 = Array.from(new Set(arr2));
+
+  let result = [];
+
+  noDuplicateArr1.forEach((number) => {
+    if (noDuplicateArr2.includes(number)) {
+      result.push(number);
+    }
+  });
+
+  return result;
+}
 
 module.exports = arrayIntersection;
