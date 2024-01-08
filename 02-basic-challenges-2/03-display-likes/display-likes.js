91f1e1689c5abb7f7ccdb61aa7831e8ccc3c0209
Rajibul Islam <rajibulski110@gmail.com>
2024-01-08 18:51:04 +0530

    my solution to the display likes challenge

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,18 @@
-function displayLikes() {}
+function displayLikes(names) {
+  const length = names.length;
+
+  if (!length) return `no one likes this`;
+
+  if (length === 1) {
+    return `${names[0]} likes this`;
+  } else if (length === 2) {
+    return `${names[0]} and ${names[1]} like this`;
+  } else if (length === 3) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  } else if (length > 3) {
+    const [name1, name2, ...rest] = names;
+    return `${name1}, ${name2} and ${rest.length} others like this`;
+  }
+}
 
 module.exports = displayLikes;
