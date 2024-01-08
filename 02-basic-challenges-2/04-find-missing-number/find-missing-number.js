0a3ab6bea9d4e94731dfa4e5e75b198b9468f7c6
Rajibul Islam <rajibulski110@gmail.com>
2024-01-08 19:35:50 +0530

    my solutiion to find missing number challenge

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,20 @@
-function findMissingNumber() {}
+function findMissingNumber(numbers) {
+  if (numbers.length === 0) return 1;
+
+  let totalIfNumberNotMissing = 0;
+  const maxNumber = Math.max(...numbers);
+  for (let i = 0; i <= maxNumber; i++) {
+    totalIfNumberNotMissing += i;
+  }
+
+  const totalWhileNumberMissing = sum(numbers);
+
+  return totalIfNumberNotMissing - totalWhileNumberMissing;
+}
+
+// helpers
+function sum(numbers) {
+  return numbers.reduce((acc, curr) => curr + acc, 0);
+}
 
 module.exports = findMissingNumber;
